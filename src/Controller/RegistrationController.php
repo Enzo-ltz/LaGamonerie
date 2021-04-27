b9c3d089ca5e3cfc783f8e8663bf78bbefe24c26
Fabien_V_LPDIM <fabien.vauclin@outlook.fr>
2021-04-27 15:14:11 +0200

    Scaffolding user entity

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    private $passwordEncoder;
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder=$passwordEncoder;
+    }
+
+    /**
+     * @Route("/registration", name="registration")
+     */
+    public function index(Request $request): Response
+    {
+        $user=new User();
+        $form=$this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $user->setRoles(["ROLE_USER"]);
+            $entity_manager=$this->getDoctrine()->getManager();
+            $entity_manager->persist($user);
+            $entity_manager->flush();
+            dd($user);
+        }
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+
+
+}
