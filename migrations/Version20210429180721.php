54826f8129d0390cf335275134d062a817f2bd9e
Enzo <enzo.lentzy.83@gmail.com>
2021-04-29 20:33:21 +0200

    :fire: clean up code

--- migrations/Version20210429180721.php
+++ migrations/Version20210429180721.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210429171418 extends AbstractMigration
+final class Version20210429180721 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -21,23 +21,25 @@ final class Version20210429171418 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE game_type (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, username VARCHAR(255) NOT NULL, bio LONGTEXT DEFAULT NULL, town VARCHAR(30) DEFAULT NULL, image VARCHAR(255) DEFAULT NULL, birthday DATE DEFAULT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE language (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(30) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user_game_type (user_id INT NOT NULL, game_type_id INT NOT NULL, INDEX IDX_672BB6E1A76ED395 (user_id), INDEX IDX_672BB6E1508EF3BC (game_type_id), PRIMARY KEY(user_id, game_type_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user_language (user_id INT NOT NULL, language_id INT NOT NULL, INDEX IDX_345695B5A76ED395 (user_id), INDEX IDX_345695B582F1BAF4 (language_id), PRIMARY KEY(user_id, language_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE user_game_type ADD CONSTRAINT FK_672BB6E1A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_game_type ADD CONSTRAINT FK_672BB6E1508EF3BC FOREIGN KEY (game_type_id) REFERENCES game_type (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_language ADD CONSTRAINT FK_345695B5A76ED395 FOREIGN KEY (user_id) REFERENCES `user` (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE user_language ADD CONSTRAINT FK_345695B582F1BAF4 FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE user ADD bio LONGTEXT DEFAULT NULL, ADD town VARCHAR(30) DEFAULT NULL, ADD image VARCHAR(255) DEFAULT NULL, ADD birthday DATE DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE user_game_type DROP FOREIGN KEY FK_672BB6E1508EF3BC');
-        $this->addSql('ALTER TABLE user_game_type DROP FOREIGN KEY FK_672BB6E1A76ED395');
-        $this->addSql('ALTER TABLE user_language DROP FOREIGN KEY FK_345695B5A76ED395');
-        $this->addSql('DROP TABLE game_type');
-        $this->addSql('DROP TABLE `user`');
-        $this->addSql('DROP TABLE user_game_type');
         $this->addSql('ALTER TABLE user_language DROP FOREIGN KEY FK_345695B582F1BAF4');
+        $this->addSql('DROP TABLE game_type');
+        $this->addSql('DROP TABLE language');
+        $this->addSql('DROP TABLE user_game_type');
+        $this->addSql('DROP TABLE user_language');
+        $this->addSql('ALTER TABLE `user` DROP bio, DROP town, DROP image, DROP birthday');
     }
 }
